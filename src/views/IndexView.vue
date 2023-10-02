c9a5135d1ecb0da563c1f049832c561a36e52ed0
devinferdynandjobs <jobsdevin2@gmail.com>
2023-10-02 13:24:53 +0700

    for commits

--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -1,49 +1,76 @@
 <template>
-  <div>
-    <center>
-      <button
-        v-if="cameraOpen === false"
-        @click="bukaKamera"
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Buka Kamera
-      </button>
-      <button
-        v-else
-        @click="tutupKamera"
-        class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Tutup Kamera
-      </button>
+  <center>
+    <div style="width: 80%">
+      <br /><br />
+      <center>
+        <button
+          v-if="cameraOpen === false"
+          @click="bukaKamera"
+          style="width: 64%"
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Buka Kamera
+        </button>
+        <button
+          v-else
+          style="width: 64%"
+          @click="tutupKamera"
+          class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+        >
+          Tutup Kamera
+        </button>
+        <br /><br />
+        <img v-if="ulangis === false" :src="imgSrc" />
+        <img v-else :src="imgSrc" /><br />
 
-      <img v-if="ulangis === false" :src="imgSrc" alt="Screenshot" />
-      <img v-else :src="imgSrc" alt="Screenshot" />
-      <video ref="videoElement" autoplay></video>
+        <video
+          ref="videoElement"
+          autoplay
+          style="border-radius: 20px; transform: scaleX(-1)"
+        ></video>
+        <br />
 
-      <button
-        @click="ambilScreenshot"
-        class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Ambil Screenshot
-      </button>
-
-      <button
-        @click="ulangi"
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-      >
-        Ulangi
-      </button>
-    </center>
-  </div>
+        <div v-if="cameras == true">
+          <span>
+            <button
+              v-if="ambilscrinsut == false"
+              @click="ambilScreenshot"
+              style="width: 32%"
+              class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Ambil Screenshot
+            </button>
+            &nbsp;
+            <button
+              @click="ulangi"
+              style="width: 32%"
+              class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Ulangi
+            </button>
+          </span>
+        </div>
+      </center>
+    </div>
+  </center>
 </template>
 
 <script>
 export default {
   data() {
-    return { stream: null, imgSrc: null, cameraOpen: false, ulangis: false };
+    return {
+      stream: null,
+      cameras: false,
+      imgSrc: null,
+      cameraOpen: false,
+      ulangis: false,
+      ambilscrinsut: false,
+    };
   },
   methods: {
     bukaKamera() {
+      this.imgSrc = null;
+      this.cameras = true;
       this.cameraOpen = true;
       navigator.mediaDevices
         .getUserMedia({ video: true })
@@ -52,7 +79,7 @@ export default {
           this.$refs.videoElement.srcObject = stream;
         })
         .catch((error) => {
-          console.error("Error accessing camera:", error);
+          alert("Error accessing camera:", error);
         });
     },
     tutupKamera() {
@@ -61,6 +88,7 @@ export default {
         this.$refs.videoElement.srcObject = null;
       }
       this.cameraOpen = false;
+      this.cameras = false;
     },
     ambilScreenshot() {
       const video = this.$refs.videoElement;
@@ -72,11 +100,14 @@ export default {
       const dataURL = canvas.toDataURL("image/png");
       this.imgSrc = dataURL;
       this.tutupKamera();
+      this.cameras = true;
+      this.ambilscrinsut = true;
     },
     ulangi() {
       this.imgSrc = null; // Menghapus gambar yang ada
       this.bukaKamera();
       this.ulangis = true;
+      this.ambilscrinsut = false;
     },
   },
 };
